5d294ebf4ee42492e91e05a94b826f25da1dc9d0
zelony <zelonym@gmail.com>
2022-04-16 00:25:30 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'user',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
